7a41361b13b5c101b8f6eda148283ea85f366e50
Anilbk777 <anilbk775@gmail.com>
2025-12-15 19:18:38 +0545

    day 3

--- app/app.py
+++ app/app.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
 
 app = FastAPI
+text_posts = {"1":{"title":"New Post", "content": "cool test post"}}
 
 @app.get("/")
 def root():
     return {"message": "Hello FastAPI"}
+
+
+@app.get("/post")
+def get_all_posts():
+    return text_posts
