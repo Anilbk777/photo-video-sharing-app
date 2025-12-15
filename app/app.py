f681834f65ba6e8120c178fd2309380ba5fe4f5b
Anilbk777 <anilbk775@gmail.com>
2025-12-15 19:22:10 +0545

    post endpoint updated

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI ,HTTPException
 
 app = FastAPI
 text_posts = {"1":{"title":"New Post", "content": "cool test post"}}
@@ -15,4 +15,7 @@ def get_all_posts():
 
 @app.get("/posts/{id}")
 def get_post(id: int):
+
+    if id not in text_posts:
+        raise HTTPException(status_code=404, details="Post Not Found")
     return text_posts.get(id)
