35ff8743b4bbd0b25d8490f9b4b54e3af91e5947
Anilbk777 <anilbk775@gmail.com>
2025-12-16 21:22:02 +0545

    updated

--- app/app.py
+++ app/app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI ,HTTPException
 
 app = FastAPI
-text_posts = {"1":{"title":"New Post", "content": "cool test post"}}
+text_posts = {
+    
+    "1":{"title":"New Post 1", "content": "cool test post 1"},
+    "2":{"title":"New Post 2", "content": "cool test post 2"},
+    "3":{"title":"New Post 3", "content": "cool test post 3"}
+
+              }
 
 @app.get("/")
 def root():
@@ -9,7 +15,9 @@ def root():
 
 
 @app.get("/post")
-def get_all_posts():
+def get_all_posts(limit: int=None):
+    if limit:
+        return text_posts[:limit]
     return text_posts
 
 
@@ -18,4 +26,6 @@ def get_post(id: int):
 
     if id not in text_posts:
         raise HTTPException(status_code=404, details="Post Not Found")
+    
     return text_posts.get(id)
+
